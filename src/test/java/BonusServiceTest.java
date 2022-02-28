b47290029658500a1b2b89a32ae67e7618802fe6
DesertScream <www.anton.dzuin@gmail.com>
2022-02-28 19:27:54 +0500

    oneiter

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -33,32 +33,34 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
-
     @org.junit.jupiter.api.Test
     void shouldCalculateForNonRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
+        // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
-        long expected = 30;
+        long expected = 10;
 
+        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
+        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
-
     }
-
     @org.junit.jupiter.api.Test
-    void shouldCalculateForNonRegisteredAndOverLimit() {
+    void shouldCalculateForNonRegisteredAndOver501Limit() {
         BonusService service = new BonusService();
 
-        long amount = 1_000_000_60;
-        boolean registered = false;
-        long expected = 30;
+        // подготавливаем данные:
+        long amount = 16700_00;
+        boolean registered = true;
+        long expected = 500;
 
+        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
+        // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
-
 }
